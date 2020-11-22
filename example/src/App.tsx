88c0c8556ebcec6553486869134ec716375dc440
211211 <nhquan307@outlook.com>
2020-11-22 17:59:20 +0700

    initial project

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'fnd-demo-v2'
+import 'fnd-demo-v2/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
